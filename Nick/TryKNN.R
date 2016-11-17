bf3747003bc4cc12b1d16577be562cf8a4aa7289
NickTalavera <contact@nicktalavera.com>
2016-11-16 19:28:25 -0500

    Renamed old data variable to new variable

--- Nick/TryKNN.R
+++ Nick/TryKNN.R
@@ -16,8 +16,8 @@ trainData = read.csv(paste0(dataFolder,'train.csv'), nrows = nrows)
 cat_var <- names(trainData)[which(sapply(trainData, is.character))]
 num_var <- names(trainData)[which(sapply(trainData, is.numeric))]
 num_var <- setdiff(num_var, c("id", "loss"))
-trainData_cat <- dt_train[,.SD, .SDcols = cat_var]
-trainData_num <- dt_train[,.SD,.SDcols = num_var]
+trainData_cat <- trainData[,.SD, .SDcols = cat_var]
+trainData_num <- trainData[,.SD,.SDcols = num_var]
 
 # trainData[, grepl("cont", names(trainData))]
 head(trainData)
