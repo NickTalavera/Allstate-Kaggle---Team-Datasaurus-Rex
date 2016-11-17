fc41ab0daaedc8bec476178cedf3a49a6410ac04
NickTalavera <contact@nicktalavera.com>
2016-11-16 19:27:40 -0500

    split data into categorical and numeric

--- Nick/TryKNN.R
+++ Nick/TryKNN.R
@@ -13,6 +13,12 @@ dataFolder = './Data/'
 nrows= 1000
 testData = read.csv(paste0(dataFolder,'test.csv'), nrows = nrows)
 trainData = read.csv(paste0(dataFolder,'train.csv'), nrows = nrows)
+cat_var <- names(trainData)[which(sapply(trainData, is.character))]
+num_var <- names(trainData)[which(sapply(trainData, is.numeric))]
+num_var <- setdiff(num_var, c("id", "loss"))
+trainData_cat <- dt_train[,.SD, .SDcols = cat_var]
+trainData_num <- dt_train[,.SD,.SDcols = num_var]
+
 # trainData[, grepl("cont", names(trainData))]
 head(trainData)
 testData$loss = NA
