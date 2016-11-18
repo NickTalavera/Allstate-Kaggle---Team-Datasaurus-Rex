06b0540e9fb547f3d0704d73e825f5ecfd2a6ade
nicktalavera <contact@nicktalavera.com>
2016-11-18 14:56:14 -0500

    Implemented Parallel to Chi Square Test

--- Nick/TryKNN.R
+++ Nick/TryKNN.R
@@ -1,4 +1,4 @@
-# rm(list = ls())
+#rm(list = ls())
 library(class)
 library(qdapRegex)
 library(kknn) #Load the weighted knn library.
@@ -9,7 +9,18 @@ library(Hmisc)
 library(stringi)
 library(dplyr)
 library(plyr)
-setwd('/Users/nicktalavera/Coding/NYC_Data_Science_Academy/Projects/Allstate-Kaggle---Team-Datasaurus-Rex')
+library(foreach)
+library(parallel)
+library(doParallel)
+cores.Number = detectCores(all.tests = FALSE, logical = TRUE)
+cl <- makeCluster(2)
+registerDoParallel(cl, cores=cores.Number)
+if (dir.exists('/Users/nicktalavera/Coding/NYC_Data_Science_Academy/Projects/Allstate-Kaggle---Team-Datasaurus-Rex')) {
+  setwd('/Users/nicktalavera/Coding/NYC_Data_Science_Academy/Projects/Allstate-Kaggle---Team-Datasaurus-Rex')
+} else if (dir.exists("~/Allstate-Kaggle---Team-Datasaurus-Rex")) {
+  setwd("~/Allstate-Kaggle---Team-Datasaurus-Rex")
+}
+
 dataFolder = './Data/'
 if (!exists("testData")) { 
   testData = read.csv(paste0(dataFolder,'test.csv'))
@@ -17,7 +28,8 @@ if (!exists("testData")) {
 if (!exists("trainData")) { 
   trainData = read.csv(paste0(dataFolder,'train.csv'))
 }
-trainData_cat <- cbind(trainData[,1:117])
+trainData_cat <- cbind(trainData[,2:117])
+trainData_catNoStates = select(trainData_cat, -cat112)
 trainData_num <- cbind(trainData[,118:ncol(trainData)])
 # head(trainData_num)
 # trainData_num.describe()
@@ -26,14 +38,18 @@ trainData_num <- cbind(trainData[,118:ncol(trainData)])
 testData$loss = NA
 
 chiSquareAllColumns = function(data){
-  chiResults = data.frame(matrix(ncol = 0, nrow = ncol(data)))
-  head(chiResults)
-  for(columnNameInner in names(data)){
-    print(paste("columnNameInner:", columnNameInner))
-    chiResults[,columnNameInner] = apply(data, 2 , function(i) chisq.test(table(data[,columnNameInner] , i ))$p.value)
-    print(summary(chiResults))
+  #chiResults = data.frame(matrix(ncol = ncol(data), nrow = ncol(data)))
+  
+  chiResults = foreach (i = 1:length(data), .combine=cbind) %dopar% {
+    columnName = colnames(data)[i]
+    print(paste("Column Name:", columnName))
+    return(columnName = apply(data, 2 , function(i) chisq.test(table(data[, columnName], i ))$p.value))
   }
-  chiResults[chiResults > 0.05] = 'Inisgnificant'
+  chiResults[chiResults > 0.05] = Inf
+  names(chiResults) = colnames(data)
   return(chiResults)
 }
-chiSquareAllColumns(trainData_num)
+a = data.frame(chiSquareAllColumns(trainData_cat))
+a = data.frame(a)
+head(a)
+stopCluster(cl)
