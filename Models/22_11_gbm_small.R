97fd4186ebc7a0794e9df80af5d729515a34656f
jlitven <jlitven@gmail.com>
2016-11-22 17:50:19 -0500

    Created smaller model

--- Models/22_11_gbm_small.R
+++ Models/22_11_gbm_small.R
@@ -1,18 +1,18 @@
 # Directory parameters
 local_dir = '~/Courses/nyc_data_science_academy/projects/Allstate-Kaggle---Team-Datasaurus-Rex/'
-server_dir = '~/'
+server_dir = '~/ML'
 
 # Model parameters
 model_method = "gbm"
-model_grid <- expand.grid( n.trees = seq(300, 400, 50), 
-                           interaction.depth = c(1, 7), 
+model_grid <- expand.grid( n.trees = 100, 
+                           interaction.depth = 1, 
                            shrinkage = 0.05,
                            n.minobsinnode = 20)
 
 # Misc Parameters
 subset_ratio = .01 # for testing purposes (set to 1 for full data)
 partition_ratio = .8 # for cross-validation
-cv_folds = 2 # for cross-validation 
+cv_folds = 5 # for cross-validation 
 
 parallelize = TRUE # parallelize the computation?
 create_submission = FALSE # create a submission for Kaggle?
@@ -22,11 +22,12 @@ metric = 'MAE' # metric use for evaluating cross-validation
 
 data_path = "Data" # data path containing train and test sets
 output_path = "Output" # output path for storing results
+group_path = "Group"
 
 if(dir.exists(local_dir)){
   setwd(local_dir)
 }else{
-  # Use Dina server
+  setwd(server_dir)
 }
 
 # Create the output directory
@@ -43,4 +44,4 @@ file.copy(sys.frame(1)$ofile,
                          paste0(model_method, ".R")))
 
 # Run the model and output results
-source('11.21_model_maker.R')
+source(file.path(group_path, 'model_maker.R'))
