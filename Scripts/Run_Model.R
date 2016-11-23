710bcbd00e823736a5d352828546e8f1fe78c982
jlitven <jlitven@gmail.com>
2016-11-23 00:25:57 -0500

    Fixed things

---
+++ Scripts/Run_Model.R
@@ -0,0 +1,116 @@
+#!/usr/bin/env Rscript
+
+# Script to run a model created from model_template.R
+# If model_file == "all", then all models in the model_output folder is run
+model_file = "xgboost_small.R" # Run this model in interactive mode
+parallelize = FALSE # parallelize models?
+
+local_dir = '~/Courses/nyc_data_science_academy/projects/Allstate-Kaggle---Team-Datasaurus-Rex/'
+server_dir = '~/ML'
+
+# Directory parameters
+data_path = "Data" # data path containing train and test sets
+output_path = "Output" # output path for storing results
+scripts_path = "Scripts" # contains the model_maker.R file
+models_path = "Models" # path to models
+
+# Get the name of the model to run
+if(!interactive()){
+  # Get input arguments
+  args = commandArgs(trailingOnly=TRUE)
+  
+  # Test if there is at least one argument: if not, return an error
+  if (length(args) == 0) {
+    stop("Usage: Run_Model [model.R]", call. = FALSE)
+  }else {
+    model_file = args[1]
+  }
+}
+
+# Add parallelization
+library(doParallel)
+if(parallelize){
+  library(doParallel)
+  cores.number = detectCores(all.tests = FALSE, logical = TRUE)
+  cl = makeCluster(2)
+  registerDoParallel(cl, cores=cores.number)
+}
+
+# Set the working directory based on local or server
+if(dir.exists(local_dir)){
+  setwd(local_dir)
+} else if(dir.exists(server_dir)) {
+  setwd(server_dir)
+}
+
+# Create the output directory
+if (!dir.exists(output_path)) {
+  print("Creating output directory..")
+  dir.create(output_path)
+}
+
+# Make sure data directory exists
+stopifnot(dir.exists('Data'))
+
+# Source the model maker
+source(file.path(scripts_path, 'model_maker.R'))
+
+# Create the results directory, run and output the model
+# input: 
+#   model_file - file name of the model
+# output:
+#   none
+run_model = function(model_file){
+  
+  # Create the time-stamped results directory
+  model_name = gsub(".R", "", model_file)
+  model_output_path = file.path(output_path, 
+                        paste0(format(Sys.time(), "%d_%m_%Y_%H.%M.%S_"), model_name))
+  dir.create(model_output_path)
+  
+  # Copy the model R file to the results directory
+  print("Copying model...")
+  file.copy(file.path(models_path, model_file), to = file.path(model_output_path, model_file))
+  
+  # Load model parameters
+  source(file.path(models_path, model_file))
+  
+  # Create a named list storing the model
+  named.list <- function(...) { 
+    l <- setNames( list(...) , as.character( match.call()[-1]) ) 
+    l
+  }
+  model_params = named.list(model_method,
+                     model_grid,
+                     partition_ratio,
+                     cv_folds,
+                     verbose_on,
+                     metric,
+                     subset_ratio,
+                     parallelize,
+                     create_submission,
+                     use_log)
+  
+  # Run the model and output results
+  make_model(model_params, data_path, model_output_path)
+}
+
+# Run all models?
+if(model_file == "all"){
+  modelFiles = list.files(path = paste0(getwd(),"/",models_path), pattern= "*.R$", full.names = FALSE, ignore.case = TRUE)
+  modelFiles = modelFiles[modelFiles != 'model_template.R']
+  
+  if(parallelize){
+    parSapply(cl, modelFiles, run_model)
+  } else {
+    sapply(modelFiles, run_model)
+  }
+}else{
+  # Run the model
+  run_model(model_file)
+}
+
+# Stop parallel clusters
+if(parallelize){
+  stopCluster(cl)
+}
