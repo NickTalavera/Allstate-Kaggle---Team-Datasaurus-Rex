bae0c0d18f9d4713824f4d50411edfc71db650e6
NickTalavera <contact@nicktalavera.com>
2016-11-22 19:01:13 -0500

    Added prefix feature to filter files

--- Models/Run_All_Models.R
+++ Models/Run_All_Models.R
@@ -1,12 +1,16 @@
+prefix = "" #The beginning of the name of the files you want to run eg. "22_11" or "" for all
+
 # Directory parameters
-local_dir = '~/Courses/nyc_data_science_academy/projects/Allstate-Kaggle---Team-Datasaurus-Rex/Josh/'
+local_dir = 'YOUR LOCAL DIRECTORY'
 server_dir = '~/ML'
 if(dir.exists(local_dir)){
   setwd(local_dir)
-} else{
+} else if(dir.exists(server_dir)) {
   setwd(server_dir)
 }
+stopifnot(dir.exists('Data'))
+
 models_dir = 'Models'
-modelFiles = list.files(path = paste0(getwd(),"/",models_dir), pattern="*.R$", full.names = TRUE, ignore.case = TRUE)
+modelFiles = list.files(path = paste0(getwd(),"/",models_dir), pattern= paste0(prefix,"*.R$"), full.names = TRUE, ignore.case = TRUE)
 modelFiles[modelFiles != 'Run_All_Models.R']
 sapply(modelFiles, source)
