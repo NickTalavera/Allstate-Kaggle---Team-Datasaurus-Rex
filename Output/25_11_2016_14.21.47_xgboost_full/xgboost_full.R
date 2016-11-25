70a70ed878e41f3bacbeebe18e07059ae8ecbbd9
jlitven <jlitven@gmail.com>
2016-11-25 18:15:34 -0500

    Fixed model maker bug

--- Output/25_11_2016_14.21.47_xgboost_full/xgboost_full.R
+++ Output/25_11_2016_14.21.47_xgboost_full/xgboost_full.R
@@ -10,13 +10,13 @@ model_grid <- expand.grid(nrounds = 2813,
 extra_params = NULL
 
 # Cross-validation parameters
-do_cv = FALSE
+do_cv = TRUE
 partition_ratio = .8 # for cross-validation
 cv_folds = 5 # for cross-validation
 verbose_on = TRUE # output cv folds results?
 metric = 'MAE' # metric use for evaluating cross-validation
 
 # Misc parameters
-subset_ratio = .5 # for testing purposes (set to 1 for full data)
+subset_ratio = .01 # for testing purposes (set to 1 for full data)
 create_submission = TRUE # create a submission for Kaggle?
 use_log = TRUE # take the log transform of the response?
